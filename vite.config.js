db5758e6af3aaf2d67f110ec48c011fff508ec83
pranaygoud01 <pranaygoudpalle123@gmail.com>
2025-10-19 12:39:37 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://bookstore-server-psi.vercel.app',
         changeOrigin: true,
         secure: false,
       },
